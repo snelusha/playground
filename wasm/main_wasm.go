145ade0b9f0f8884876b97e0a1ffbc13f24cb943
Sithija Nelusha Silva <hello@snelusha.dev>
2026-03-03 10:45:24 +0530

    Add WASM runtime with localStorageFS

---
+++ wasm/main_wasm.go
@@ -0,0 +1,81 @@
+// Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com).
+//
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import (
+	_ "ballerina-lang-go/lib/rt"
+	"ballerina-lang-go/projects"
+	"ballerina-lang-go/projects/directory"
+	"ballerina-lang-go/runtime"
+	"fmt"
+	"syscall/js"
+)
+
+func main() {
+	js.Global().Set("run", js.FuncOf(run))
+	select {}
+}
+
+func run(this js.Value, args []js.Value) any {
+	if len(args) < 2 {
+		return jsError(fmt.Errorf("expected at least 2 arguments: (fsProxy, path)"))
+	}
+
+	proxy := args[0]
+	path := args[1].String()
+
+	fsys := NewLocalStorageFS(proxy)
+
+	result, err := directory.LoadProject(fsys, path)
+	if err != nil {
+		return jsError(err)
+	}
+
+	diags := result.Diagnostics()
+	if diags.HasErrors() {
+		return nil
+	}
+
+	project := result.Project()
+	pkg := project.CurrentPackage()
+
+	compilation := pkg.Compilation()
+	diags = compilation.DiagnosticResult()
+	if diags.HasErrors() {
+		return nil
+	}
+
+	backend := projects.NewBallerinaBackend(compilation)
+	bir := backend.BIR()
+
+	if bir == nil {
+		return jsError(fmt.Errorf("BIR generation failed"))
+	}
+
+	rt := runtime.NewRuntime()
+	if err := rt.Interpret(*bir); err != nil {
+		return jsError(err)
+	}
+
+	return nil
+}
+
+func jsError(err error) map[string]any {
+	return map[string]any{
+		"error": err.Error(),
+	}
+}
