aec015f0ee3436451657184e83806dca5001b49c
snelusha <hello@snelusha.dev>
2026-03-21 14:53:15 +0530

    Refactor app component into splat route

--- web/src/routes/$.tsx
+++ web/src/routes/$.tsx
@@ -1,7 +1,24 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import App from "@/app";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+import { FSProvider } from "@/providers/fs-provider";
+
+import { FileRouteSync } from "@/components/file-route-sync";
+import { Editor } from "@/components/editor";
 
 export const Route = createFileRoute("/$")({
-	component: App,
+	component: SplatComponent,
 });
+
+function SplatComponent() {
+	return (
+		<TooltipProvider>
+			<FSProvider>
+				<FileRouteSync>
+					<Editor />
+				</FileRouteSync>
+			</FSProvider>
+		</TooltipProvider>
+	);
+}
