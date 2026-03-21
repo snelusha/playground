0b39d633983bff135e2cc08bc6bffb20e61f1c05
snelusha <hello@snelusha.dev>
2026-03-21 12:02:28 +0530

    Address review suggesitions

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -19,6 +19,12 @@ const router = createRouter({
 	defaultPreload: "intent"
 });
 
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<RouterProvider router={router} />
